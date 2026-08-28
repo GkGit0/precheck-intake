3701c41dfc0e491a0fadec9808645fa3ba85278e
Gaku <kugashihakataa@gmail.com>
2026-08-28 09:54:16 +0900

    Step4: ベトナム語・インドネシア語の翻訳データを追加

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,9 +1,11 @@
 import en from "./en.json";
 import zh from "./zh.json";
+import vi from "./vi.json";
+import id from "./id.json";
 
 // To add a language: create src/locales/<code>.json (copy en.json as a template),
 // then import it here and add it to this object. No other code changes are needed.
-export const locales = { en, zh };
+export const locales = { en, zh, vi, id };
 
 export type LangCode = keyof typeof locales;
 export type Strings = (typeof locales)[LangCode];
